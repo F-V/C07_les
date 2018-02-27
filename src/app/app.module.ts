1b74cfea95fa288442d537e136e5c4e5b9d7f3c2
F-V <32668262+F-V@users.noreply.github.com>
2018-02-27 14:05:30 +0100

    Config firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,19 @@ import { AuthProvider } from '../providers/auth/auth';
 import { StoreProvider } from '../providers/store/store';
 import { OrderTabsPage } from '../pages/order-tabs/order-tabs';
 
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth, AngularFireAuthModule } from 'angularfire2/auth';
+import { UserServiceProvider } from '../providers/user-service/user-service';
 
+var config = {
+  apiKey: "AIzaSyCfORbG__nTUiaIFkL6_55XKWNZSC5zCck",
+  authDomain: "mobiele-c06a4.firebaseapp.com",
+  databaseURL: "https://mobiele-c06a4.firebaseio.com",
+  projectId: "mobiele-c06a4",
+  storageBucket: "mobiele-c06a4.appspot.com",
+  messagingSenderId: "1030775454063"
+};
 
 @NgModule({
   declarations: [
@@ -24,7 +36,10 @@ import { OrderTabsPage } from '../pages/order-tabs/order-tabs';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -38,7 +53,8 @@ import { OrderTabsPage } from '../pages/order-tabs/order-tabs';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     AuthProvider,
-    StoreProvider
+    StoreProvider,
+    UserServiceProvider
   ]
 })
 export class AppModule {}
